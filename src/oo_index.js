cd87589732ae9ae97b0666b34e872a33d95cd8ee
Chris Stull <chris.stull@gmail.com>
2018-10-15 13:16:07 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -2,3 +2,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 });
+
+let tasks = []
--- src/task.js
+++ src/task.js
@@ -1,2 +1,10 @@
+taskID = 0
+
 class Task {
+  constructor(description, priority){
+    this.description = description
+    this.priority = priority
+    this.id = ++taskID
+    tasks.push(this)
+  }
 }
