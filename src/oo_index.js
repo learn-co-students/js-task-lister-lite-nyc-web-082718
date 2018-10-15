8a58db484f7a4eb000f70bb81387418246d7b2aa
yammik <mkim0514@gmail.com>
2018-10-15 15:04:57 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  const button = document.getElementById('submit');
+
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    const input = document.getElementById('new-task-description').value;
+    taskList.newTask(input);
+    document.getElementById('new-task-description').value = '';
+    // e.target.reset();
+  });
 });
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,23 @@
 class TaskList {
+
+  showTask(task) {
+    const taskLi = document.createElement('li');
+    taskLi.innerText = task.description + "   ";
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = "X";
+    deleteButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('tasks').removeChild(this.parentElement);
+    });
+
+    taskLi.appendChild(deleteButton);
+
+    document.getElementById('tasks').appendChild(taskLi);
+  }
+
+  newTask(description) {
+    const task = new Task(description);
+
+    this.showTask(task);
+  }
 }
