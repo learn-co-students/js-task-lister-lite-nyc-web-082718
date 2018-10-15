7bd9c7679265e8492f3713e983b261cb28151851
MarSchlaefer <mschlaefer00@gmail.com>
2018-10-15 11:49:11 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+  taskList.grabSubmit().addEventListener('click', e => {
+
+    e.preventDefault()
+    let userInput = taskList.grabUserInput().value
+
+    taskList.grabTask().innerHTML += `<li> ${userInput} </li>`
+
+    taskList.grabUserInput().value = ''
+  })
+
+
 });
